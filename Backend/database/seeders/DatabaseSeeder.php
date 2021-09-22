0be40ca7330bf51e692d3a081cf594fc49f166b6
Vo Quoc Dat <dat.voquoc@vti.com.vn>
2021-09-22 20:55:25 +0900

    update seeder

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use TCG\Voyager\Models\DataRow;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            VoyagerDatabaseSeeder::class,
+            VoyagerDummyDatabaseSeeder::class,
+        ]);
     }
 }
